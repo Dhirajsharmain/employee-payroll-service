6a1566ae1be987ad5c908915c757fbdfd26c2644
Dhiraj Sharma <dhirajsharmads0@gmail.com>
2021-06-29 21:14:43 +0530

    added : program to demonstrate file operations

--- bridgelabz/service/EmployeePayrollService.java
+++ bridgelabz/service/EmployeePayrollService.java
@@ -19,9 +19,15 @@ package bridgelabz.service;
 import bridgelabz.exception.EmployeePayrollValidation;
 import bridgelabz.model.EmployeePayrollData;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class EmployeePayrollService {
     private List<EmployeePayrollData> employeePayrollList;
@@ -38,16 +44,18 @@ public class EmployeePayrollService {
      *
      * @param args
      */
-    public static void main(String[] args) throws EmployeePayrollValidation {
+    public static void main(String[] args) throws EmployeePayrollValidation, IOException {
 
         ArrayList<EmployeePayrollData> employeePayrollList = new ArrayList<>();
-
         EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
-
         Scanner scanner = new Scanner(System.in);
 
+        System.out.println(employeePayrollService.isExists("E:\\BridgeLabs Training\\demo2"));
         employeePayrollService.readEmployeePayrollData(scanner);
         employeePayrollService.writeEmployeePayrollData();
+
+        employeePayrollService.listFileAndDirectories("E:\\BridgeLabs Training\\demo2");
+        employeePayrollService.createFile("E:\\BridgeLabs Training\\demo2", "Myfile");
     }
 
     /**
@@ -67,15 +75,10 @@ public class EmployeePayrollService {
         try {
 
             System.out.println("Enter Employee ID: ");
-
             int id = scanner.nextInt();
-
             System.out.println("Enter Employee Name: ");
-
             String name = scanner.next();
-
             System.out.println("Enter Employee Salary: ");
-
             double salary = scanner.nextDouble();
 
             employeePayrollList.add(new EmployeePayrollData(id, name, salary));
@@ -83,6 +86,97 @@ public class EmployeePayrollService {
             throw new EmployeePayrollValidation(e.getMessage());
         }
 
+    }
+
+    /**
+     * Method for deleting file form directory.
+     * @param contentToDelete :
+     * @return : tru or false
+     */
+    public boolean deleteFile(File contentToDelete) {
+        File[] allContent = contentToDelete.listFiles();
+        if (allContent != null)
+            for (File file : allContent) {
+                deleteFile(file);
+            }
+        return contentToDelete.delete();
+    }
+
+    /**
+     * Method for delete files recursively  from a directory.
+     * @param address : directory path
+     * @return : true or false
+     */
+    public boolean deleteFilesRecursively(String address) {
+        Path playPath = Paths.get(address);
+        if (Files.exists(playPath))
+            deleteFile(playPath.toFile());
+        return Files.notExists(playPath);
+    }
+
+    /**
+     * Method for checking if file of directory and file is exists or not.
+     *
+     * @return : true or false
+     */
+    public boolean isExists(String address) {
+        Path homePath = Paths.get(address);
+        if (Files.exists(homePath)) {
+            System.out.println(homePath + " Path is Exists");
+        }
+        return Files.exists(homePath);
+    }
+
+    /**
+     * Method for creating directory.
+     *
+     * @param address : path wtth directory name
+     * @return : true or false
+     * @throws IOException
+     */
+    public boolean createDirectory(String address) throws IOException {
+        Path playPath = Paths.get(address);
+        // Create Directory
+        Files.createDirectory(playPath);
+        if (Files.exists(playPath))
+            System.out.println(playPath + " Is Created");
+        return Files.exists(playPath);
+    }
+
+    /**
+     * Method for creating file in a particular directory.
+     *
+     * @param address  : path of directory
+     * @param fileName : file name
+     * @return : true or false
+     * @throws EmployeePayrollValidation
+     */
+    public boolean createFile(String address, String fileName) throws EmployeePayrollValidation {
+        Path tempFile = Paths.get(address + "/" + fileName);
+        try {
+            Files.createFile(tempFile);
+            System.out.println(tempFile);
+        } catch (IOException e) {
+            throw new EmployeePayrollValidation(e.getMessage());
+        }
+        return Files.exists(tempFile);
+    }
+
+    /**
+     * Method for find out the total files and folder at a particular directory.
+     * @param path : path for a directory
+     * @return : count of file and folders exists.
+     * @throws EmployeePayrollValidation
+     */
+    public int listFileAndDirectories(String path) throws EmployeePayrollValidation {
+        Path playPath = Paths.get(path);
+        try {
+            List<Path> elementsList = Files.list(playPath).filter(Files::isRegularFile).collect(Collectors.toList());
+            elementsList.stream().forEach(e -> System.out.println(e));
+            return elementsList.size();
+        } catch (Exception e) {
+            throw new EmployeePayrollValidation(e.getMessage());
+        }
     }
 
 }
--- bridgelabz/test/EmployeePayrollServiceTest.java
+++ bridgelabz/test/EmployeePayrollServiceTest.java
@@ -1,15 +1,59 @@
 package bridgelabz.test;
 
+import bridgelabz.exception.EmployeePayrollValidation;
 import bridgelabz.service.EmployeePayrollService;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.IntStream;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 public class EmployeePayrollServiceTest {
 
+    public static String Home = System.getProperty("user.home");
     EmployeePayrollService employeePayrollService;
 
     @Before
     public void setup(){
         employeePayrollService = new EmployeePayrollService();
+    }
+
+    @Test
+    public void listFilesAndDirectories_shouldReturnFilesCount_whenPathIsProvided() throws EmployeePayrollValidation {
+        int result = employeePayrollService.listFileAndDirectories("E:\\BridgeLabs Training\\demo");
+        assertEquals(9,result);
+    }
+
+    @Test
+    public void givenPathWhenCheckedThenConfirm() throws IOException {
+
+        //check file exists
+        Path playPath = Paths.get("E:\\BridgeLabs Training\\demo2");
+        assertTrue(Files.exists(playPath));
+
+        //Delete file and check file not exists
+        if (Files.exists(playPath))
+            employeePayrollService.deleteFile(playPath.toFile());
+        assertTrue(Files.notExists(playPath));
+
+        // Create Directory
+        Files.createDirectory (playPath);
+        assertTrue (Files.exists(playPath));
+
+        // Create File
+        IntStream.range(1, 10).forEach(cntr -> {
+            Path tempFile = Paths.get(playPath + "/temp" + cntr);
+            assertTrue(Files.notExists(tempFile));
+            try {
+                Files.createFile(tempFile);
+            } catch (IOException e) { }
+            assertTrue(Files.exists(tempFile));
+        });
     }
 }
